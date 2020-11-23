f4342bb194f05c25f2917b2b61b48ecc575a2617
Yb <3392374529@qq.com>
2020-11-23 19:31:46 +0800

    no message

--- router/page.js
+++ router/page.js
@@ -3,39 +3,56 @@
 var exp = require('express')
 var app = exp()
 var router = exp.Router()
-var { User } = require('../libs/mongoose')
-
+var {User} = require('../libs/mongoose')
+var fs = require('fs')
 
 // 首页
-router.get('/', (req, res) => {
-        res.render('index')
-    })
-    // 首页-包月鲜花
-router.get('/monthly', (req, res) => {
-        res.render('monthly')
-    })
-    // 首页-礼品花束
-router.get('/gift', (req, res) => {
-        res.render('gift')
-    })
-    // 绿植多肉
-router.get('/fleshiness', (req, res) => {
-        res.render('fleshiness')
-    })
-    // F+制物所
-router.get('/moreF', (req, res) => {
-        res.render('moreF')
-    })
-    // 家居生活
-router.get('/home', (req, res) => {
-        res.render('home')
-    })
-    // 发现
-router.get('/find', (req, res) => {
-        res.render('find')
-    })
+router.get('/',(req,res)=>{
+      res.render('index')
+})
+// 首页-包月鲜花
+router.get('/monthly',(req,res)=>{
+    res.render('monthly')
+})
+// 首页-礼品花束
+router.get('/gift',(req,res)=>{
+    res.render('gift')
+})
+// 绿植多肉
+router.get('/fleshiness',(req,res)=>{
+    res.render('fleshiness')
+})
+// F+制物所
+router.get('/moreF',(req,res)=>{
+    fs.readFile('./www/moreF.json',(err,data)=>{
+        if(!err){
+          var data = JSON.parse(data)
+          res.render('moreF',{data})
+        }else{
+          console.log(err)
+        }
+     })
+})
+// 家居生活
+router.get('/home',(req,res)=>{
+    fs.readFile('./www/home.json',(err,data)=>{
+        if(!err){
+          var data = JSON.parse(data)
+   
+        res.render('home',{data})
+        }else{
+          console.log(err)
+        }
+     })
+})
+// 发现
+router.get('/find',(req,res)=>{
+    res.render('find')
+})
+
     // 我的
 router.get('/mine', (req, res) => {
+
     res.render('mine')
 })
 
