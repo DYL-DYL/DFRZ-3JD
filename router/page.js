cccc79fde743ee358c4f9fec45644f1fc5d4c184
洪国泰 <17805981973@163.com>
2020-11-23 16:27:37 +0800

    新加login页面和registor页面

--- router/page.js
+++ router/page.js
@@ -1,44 +1,42 @@
-
 // 所有的页面跳转
 // 引入模板配置
 var exp = require('express')
 var app = exp()
 var router = exp.Router()
-var {User} = require('../libs/mongoose')
+var { User } = require('../libs/mongoose')
 
 
 // 首页
-router.get('/',(req,res)=>{
-      res.render('index')
-})
-// 首页-包月鲜花
-router.get('/monthly',(req,res)=>{
-    res.render('monthly')
-})
-// 首页-礼品花束
-router.get('/gift',(req,res)=>{
-    res.render('gift')
-})
-// 绿植多肉
-router.get('/fleshiness',(req,res)=>{
-    res.render('fleshiness')
-})
-// F+制物所
-router.get('/moreF',(req,res)=>{
-    res.render('moreF')
-})
-// 家居生活
-router.get('/home',(req,res)=>{
-    res.render('home')
-})
-// 发现
-router.get('/find',(req,res)=>{
-    res.render('find')
-})
-// 我的
-router.get('/mine',(req,res)=>{
+router.get('/', (req, res) => {
+        res.render('index')
+    })
+    // 首页-包月鲜花
+router.get('/monthly', (req, res) => {
+        res.render('monthly')
+    })
+    // 首页-礼品花束
+router.get('/gift', (req, res) => {
+        res.render('gift')
+    })
+    // 绿植多肉
+router.get('/fleshiness', (req, res) => {
+        res.render('fleshiness')
+    })
+    // F+制物所
+router.get('/moreF', (req, res) => {
+        res.render('moreF')
+    })
+    // 家居生活
+router.get('/home', (req, res) => {
+        res.render('home')
+    })
+    // 发现
+router.get('/find', (req, res) => {
+        res.render('find')
+    })
+    // 我的
+router.get('/mine', (req, res) => {
     res.render('mine')
 })
 
-module.exports=router
-
+module.exports = router
