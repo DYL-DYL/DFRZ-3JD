2806b945925dfed17fa37fdf22a10df278d1e936
洪国泰 <17805981973@163.com>
2020-11-24 15:50:19 +0800

    社区部分及图片

--- www/data/communityData.js
+++ www/data/communityData.js
@@ -1,1 +1,4 @@
-var topUser = [{ pic1: '/imges/社区-首页/photo1.jpg', pic2: '/imges/社区-首页/photo2.jpg', name: 'Phoenix' }]
+var topUser = [{ pic1: '/imges/社区-首页/photo1.jpg', pic2: '/imges/社区-首页/photo2.jpg', name: 'Phoenix', job: '花艺摄影达人', user: '/imges/社区-首页/user1.jpg' }, { pic1: '/imges/社区-首页/photo3.jpg', pic2: '/imges/社区-首页/photo4.jpg', name: '甜妈', job: '花艺摄影达人', user: '/imges/社区-首页/user2.jpg' }, { pic1: '/imges/社区-首页/photo5.jpg', pic2: '/imges/社区-首页/photo6.jpg', name: 'Echo', job: '花艺造型达人', user: '/imges/社区-首页/user3.jpg' }, { pic1: '/imges/社区-首页/photo7.jpg', pic2: '/imges/社区-首页/photo8.jpg', name: '汤啊Town', job: '花艺摄影达人', user: '/imges/社区-首页/user4.jpg' }]
+
+
+var news = []
