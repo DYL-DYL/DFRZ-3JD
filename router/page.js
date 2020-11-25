ab431bf7f9e4eb78b7f78ba2cfe67fa1cbbc68be
Yb <3392374529@qq.com>
2020-11-25 19:56:17 +0800

    养护二级页面

--- router/page.js
+++ router/page.js
@@ -9,96 +9,97 @@ const { JSONCookie } = require('cookie-parser')
 
 // 首页
 router.get('/', (req, res) => {
-        res.render('index')
-    })
-    // 首页-包月鲜花
+  res.render('index')
+})
+// 首页-包月鲜花
 router.get('/monthly', (req, res) => {
-        res.render('monthly')
-    })
-    // 首页-礼品花束
+  res.render('monthly')
+})
+// 首页-礼品花束
 router.get('/gift', (req, res) => {
-        res.render('gift')
-    })
-    // 绿植多肉
+  res.render('gift')
+})
+// 绿植多肉
 router.get('/fleshiness', (req, res) => {
-        res.render('fleshiness')
-    })
-    // F+制物所
+  res.render('fleshiness')
+})
+// F+制物所
 router.get('/moreF', (req, res) => {
-        fs.readFile('./www/moreF.json', (err, data) => {
-            if (!err) {
-                var data = JSON.parse(data)
-                res.render('moreF', { data })
-            } else {
-                console.log(err)
-            }
-        })
-    })
-    // 家居生活
+  fs.readFile('./www/moreF.json', (err, data) => {
+    if (!err) {
+      var data = JSON.parse(data)
+      res.render('moreF', { data })
+    } else {
+      console.log(err)
+    }
+  })
+})
+// 家居生活
 router.get('/home', (req, res) => {
-        fs.readFile('./www/home.json', (err, data) => {
-            if (!err) {
-                var data = JSON.parse(data)
+  fs.readFile('./www/home.json', (err, data) => {
+    if (!err) {
+      var data = JSON.parse(data)
 
-                res.render('home', { data })
-            } else {
-                console.log(err)
-            }
-        })
-    })
-    // 发现
+      res.render('home', { data })
+    } else {
+      console.log(err)
+    }
+  })
+})
+// 发现
 router.get('/find', (req, res) => {
-    res.render('find')
+  res.render('find')
 })
 
 // 我的
 router.get('/mine', (req, res) => {
 
-        res.render('mine')
-    })
-    //社区
+  res.render('mine')
+})
+//社区
 router.get('/community', (req, res) => {
-        fs.readFile('www/data/news.json', (err, data) => {
-            if (!err) {
-                data = JSON.parse(data)
-                res.render('community', { data })
-            } else {
-                console.log(err)
-            }
+  fs.readFile('www/data/news.json', (err, data) => {
+    if (!err) {
+      data = JSON.parse(data)
+      res.render('community', { data })
+    } else {
+      console.log(err)
+    }
 
-        })
+  })
 
-    })
-    //鲜花养护
+})
+//鲜花养护
 router.get('/maintenance', (req, res) => {
-        res.render('maintenance')
-    })
-    //我的花束
+  res.render('maintenance')
+})
+//我的花束
 router.get('/myflower', (req, res) => {
-    res.render('myflower')
+  res.render('myflower')
 })
 router.get('/flower_detail', (req, res) => {
-  fs.readFile('./www/flower_detail.json',(err,data)=>{
-      if(!err){
-        var data = JSON.parse(data)
-        res.render('flower_detail',data[req.query.id-1])
-      }else{
-        console.log(err)
-      }
-    })
+  fs.readFile('./www/flower_detail.json', (err, data) => {
+    if (!err) {
+      var data = JSON.parse(data)
+      res.render('flower_detail', data[req.query.id - 1])
+    } else {
+      console.log(err)
+    }
+  })
 })
 //鲜花养护-单品花束
 router.get('/single_bouquet', (req, res) => {
-    fs.readFile('./www/flower.json',(err,data)=>{
-        if(!err){
-          var data = JSON.parse(data)
-          res.render('single_bouquet',data)
-        }else{
-          console.log(err)
-        }
-      })
+  fs.readFile('./www/flower.json', (err, data) => {
+    if (!err) {
+      var data = JSON.parse(data)
+      res.render('single_bouquet', data)
+    } else {
+      console.log(err)
+    }
+  })
 
 })
+
 //鲜花养护-混合
 router.get('/mix', (req, res) => {
   res.render('mix')
@@ -110,11 +111,11 @@ router.get('/mini', (req, res) => {
 
 //鲜花养护-主题
 router.get('/gift_bouquet', (req, res) => {
-  fs.readFile('./www/flower.json',(err,data)=>{
-    if(!err){
+  fs.readFile('./www/flower.json', (err, data) => {
+    if (!err) {
       var data = JSON.parse(data)
-      res.render('gift_bouquet',data)
-    }else{
+      res.render('gift_bouquet', data)
+    } else {
       console.log(err)
     }
   })
@@ -122,49 +123,53 @@ router.get('/gift_bouquet', (req, res) => {
 
 //鲜花养护-绿植
 router.get('/green_planting', (req, res) => {
-  fs.readFile('./www/flower.json',(err,data)=>{
-    if(!err){
+  fs.readFile('./www/flower.json', (err, data) => {
+    if (!err) {
       var data = JSON.parse(data)
-      res.render('green_planting',data)
-    }else{
+      res.render('green_planting', data)
+    } else {
       console.log(err)
     }
   })
 })
+//社区花朵最里面的详情
 
+router.get('/single_detail', (req, res) => {
+  res.render('single_detail')
+})
 //搜索
 router.get('/search', (req, res) => {
-    res.render('search')
+  res.render('search')
 })
 
 //社区部分-发布信息
 router.get('/community/talk', (req, res) => {
-        res.render('community-talk')
-    })
-    //社区部分-我
+  res.render('community-talk')
+})
+//社区部分-我
 router.get('/community/me', (req, res) => {
-        res.render('community-me')
-    })
-    //社区部分-消息
+  res.render('community-me')
+})
+//社区部分-消息
 router.get('/community/message', (req, res) => {
-    res.render('community-message')
+  res.render('community-message')
 })
 
 
 router.get('/register', (req, res) => {
-    res.render('register')
+  res.render('register')
 })
 
 // 详情页
-router.get('/detailPage',(req,res)=>{
-    // console.log(req.query);
-    fs.readFile('./www/data/detailPage.json',(err,data)=>{
-        if(!err){
-            var thisData=JSON.parse(data)
-            res.render('detailPage',thisData[req.query.id])
-        }else{
-            console.log(err);
-        }
-    })
+router.get('/detailPage', (req, res) => {
+  // console.log(req.query);
+  fs.readFile('./www/data/detailPage.json', (err, data) => {
+    if (!err) {
+      var thisData = JSON.parse(data)
+      res.render('detailPage', thisData[req.query.id])
+    } else {
+      console.log(err);
+    }
+  })
 })
 module.exports = router
